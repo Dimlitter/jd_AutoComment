75c92195026d55ac7f92546fcee0748a298ffe4b
viiber <viiber.tang@gmail.com>
2022-02-15 16:55:57 +0800

    Supplement and improve the request header

--- jdspider.py
+++ jdspider.py
@@ -25,7 +25,21 @@ class JDSpider:
             quote(categlory))
         self.commentBaseUrl = "https://sclub.jd.com/comment/productPageComments.action?"
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36', }
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language':'zh-CN,zh;q=0.9',
+            'cache-control': 'max-age=0',
+            'dnt':'1',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode':' navigate',
+            'sec-fetch-site':'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
+            }
         self.productsId = self.getId()
         self.comtype = {1: "nagetive", 2: "medium", 3: "positive"}
         self.categlory = categlory
